5c6b9e7083a913bd5554e45b211f3f4d879ca65f
lilleyz7 <lilley5>
2024-11-24 19:40:12 -0800

    User serializer

---
+++ authapp/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta(object):
+        model = CustomUser
+        fields = ['id', 'username', 'password', 'email']
